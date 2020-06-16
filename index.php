df895333a7a70b68d92838c58bc65084fbb3968a
karand1705 <58079053+karand1705@users.noreply.github.com>
2020-06-17 02:26:35 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>  
+  
+<head>  
+    <title>  
+        first task webpage 
+    </title>  
+</head>
+
+<body bgcolor="orange">  
+   <center>
+   <h1 >welcome to my server</h1>
+ <?php
+ print `ifconfig`
+ print `free -m`
+ ?>
+   </center>
+</body>
+</html>
